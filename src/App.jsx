4f5ad5db574bce6104819a7f6c741b29bcd24e4c
vivan raj <vivanrajmittakodi6@gmail.com>
2024-01-02 08:32:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-import React,{useEffect,useState} from 'react';
+import React,{useCallback, useEffect,useMemo,useState} from 'react';
 import './App.css';
 
 // Do not change this
@@ -15,17 +14,17 @@ function App() {
 
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const delayFunction = ()=> {
+  const delayFunction = useMemo(()=> {
     console.log("Delay Function Ran")
     for(let index=0; index<LARGE_NUMBER; index++){};
     return value+2;
   
-  }
+  },[value])
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const testFunction = ()=>{
+  const testFunction = useCallback(()=>{
     return [value*3 ,value*4]
-  }
+  },[value])
 
   
 
@@ -69,7 +68,7 @@ function App() {
       <h1 >{value}</h1>
       <button onClick={handleChangeValue}>Change Value</button>
       <button onClick={handleList}>Show List</button>
-      <h2>{delayFunction()}</h2>
+      <h2>{delayFunction}</h2>
       <div>
         {currentList.map((item,index)=>{
           return <h2 key={index}>{item}</h2>
